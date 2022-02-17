1b3405add5b7c220ba3034cc23e5dc56e31e079e
Shahadat Hussain <shussain@legalsimpli.com>
2022-02-18 00:12:19 +0530

    Minify and and updated portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -14,7 +14,7 @@ class Portfolio extends Component{
                                         <img src="./images/port-1.jpg" loading="lazy" alt="" />
                                         <div className="work-overlay">
                                             <div className="work-overlay--content">
-                                                <h5 className="work-item--text item-title">PDFSimpli</h5>
+                                                <h5 className="work-item--text item-title">PDFSimpli.com</h5>
                                                 <h5 className="work-item--text item-description">Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.</h5>
                                                 <button className="work-item--text item-btn" type="button" title="PDFSimpli.com">View Project</button>
                                             </div>
@@ -32,8 +32,41 @@ class Portfolio extends Component{
                                             <img src="./images/port-2.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">The Quint</h4>
+                                                    <h4 className="work-item--text item-title">Quint Digital Media Limited</h4>
                                                     <h5 className="work-item--text item-description">Created component-based layout, JavaScript library ReactJS, Grids susy grids, CSS pre-processor SCSS/SASS.</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="TheQuint.com">View Project</button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </Scene>
+                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
+                                <div className="work-half--item fade-effect">
+                                    <div className="hover-effect">
+                                        <a href="https://signsimpli.com/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-12.jpg" loading="lazy" alt="" />
+                                            <div className="work-overlay">
+                                            <div className="work-overlay--content">
+                                                    <h5 className="work-item--text item-title">SignSimpli.com</h5>
+                                                    <h5 className="work-item--text item-description">Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="PDFSimpli.com">View Project</button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </Scene>
+                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
+                                <div className="work-half--item fade-effect">
+                                    <div className="hover-effect">
+                                        <a href="https://uilab.in/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-13.jpg" loading="lazy" alt="" />
+                                            <div className="work-overlay">
+                                                <div className="work-overlay--content">
+                                                    <h4 className="work-item--text item-title">UILab.in</h4>
+                                                    <h5 className="work-item--text item-description">Created component-based layout, JavaScript library ReactJS, CSS pre-processor SCSS, CSS Animations and GitHub.</h5>
                                                     <button className="work-item--text item-btn" type="button" title="TheQuint.com">View Project</button>
                                                 </div>
                                             </div>
@@ -49,7 +82,7 @@ class Portfolio extends Component{
                                             <img src="./images/port-3.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Care4life</h4>
+                                                    <h4 className="work-item--text item-title">Care4Life</h4>
                                                     <h5 className="work-item--text item-description">Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.</h5>
                                                     <button className="work-item--text item-btn" type="button" title="Care4life.com">View Project</button>
                                                 </div>
@@ -65,9 +98,25 @@ class Portfolio extends Component{
                                             <img src="./images/port-4.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Text4baby</h4>
+                                                    <h4 className="work-item--text item-title">Text4Baby</h4>
                                                     <h5 className="work-item--text item-description">Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.</h5>
                                                     <button className="work-item--text item-btn" type="button" title="Text4Baby.com">View Project</button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </Scene>
+                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
+                                <div className="work-half--item fade-effect">
+                                    <div className="hover-effect">
+                                        <a href="https://beyondtheorphanage.org/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-11.jpg" loading="lazy" alt="" />
+                                            <div className="work-overlay">
+                                                <div className="work-overlay--content">
+                                                    <h4 className="work-item--text item-title">Beyond the Orphanage</h4>
+                                                    <h5 className="work-item--text item-description">Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Swiper Slider.</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="BeyondTheOrphanage.org">View Project</button>
                                                 </div>
                                             </div>
                                         </a>
@@ -81,7 +130,7 @@ class Portfolio extends Component{
                                             <img src="./images/port-5.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Buri Ladki</h4>
+                                                    <h4 className="work-item--text item-title">Buri Ladki - The Quint</h4>
                                                     <h5 className="work-item--text item-description">Who is a 'Buri Ladki'? You decide.
                                                     Developed interactive game with full screen slider.</h5>
                                                     <button className="work-item--text item-btn" type="button" title="BuriLadkiGame">View Project</button>
@@ -98,25 +147,9 @@ class Portfolio extends Component{
                                             <img src="./images/port-6.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Braahmam</h4>
+                                                    <h4 className="work-item--text item-title">Braahmam Net Solutions Pvt Ltd</h4>
                                                     <h5 className="work-item--text item-description">Converted PSD to WordPress, theme customization, manage widget and plugins.</h5>
                                                     <button className="work-item--text item-btn" type="button" title="Braahmam.com">View Project</button>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </Scene>
-                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
-                                <div className="work-half--item fade-effect">
-                                    <div className="hover-effect">
-                                        <a href="https://www.thequint.com/quintlab/sandesh-to-a-soldier/" target="_blank" rel="noreferrer nofollow">
-                                            <img src="./images/port-7.jpg" loading="lazy" alt="" />
-                                            <div className="work-overlay">
-                                                <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Sandesh to a Soldier - The Quint</h4>
-                                                    <h5 className="work-item--text item-description">Developed a full screen parallax microsite to send a message to the Soldier.</h5>
-                                                    <button className="work-item--text item-btn" type="button" title="SandeshtoaSoldier">View Project</button>
                                                 </div>
                                             </div>
                                         </a>
@@ -175,13 +208,46 @@ class Portfolio extends Component{
                             <Scene classToggle="fade-up" reverse={true} offset={-300}>
                                 <div className="work-half--item fade-effect">
                                     <div className="hover-effect">
-                                        <a href="https://beyondtheorphanage.org/" target="_blank" rel="noreferrer nofollow">
-                                            <img src="./images/port-11.jpg" loading="lazy" alt="" />
+                                        <a href="https://www.thequint.com/quintlab/sandesh-to-a-soldier/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-7.jpg" loading="lazy" alt="" />
                                             <div className="work-overlay">
                                                 <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Beyond the Orphanage</h4>
-                                                    <h5 className="work-item--text item-description">Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Swiper Slider.</h5>
-                                                    <button className="work-item--text item-btn" type="button" title="BeyondTheOrphanage.org">View Project</button>
+                                                    <h4 className="work-item--text item-title">Sandesh to a Soldier - The Quint</h4>
+                                                    <h5 className="work-item--text item-description">Developed a full screen parallax microsite to send a message to the Soldier.</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="SandeshtoaSoldier">View Project</button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </Scene>
+
+                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
+                                <div className="work-half--item fade-effect">
+                                    <div className="hover-effect">
+                                        <a href="https://www.thequint.com/quintlab/ampstories/2-point-0-movie-interesting-facts/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-amp-1.jpg" loading="lazy" alt="" />
+                                            <div className="work-overlay">
+                                                <div className="work-overlay--content">
+                                                    <h4 className="work-item--text item-title">About 2.0 Movie - AMP Story</h4>
+                                                    <h5 className="work-item--text item-description">Develop component-based layout,  Card based layout, Accelerated Mobile Pages (AMP).</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="SandeshtoaSoldier">View Project</button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </Scene>
+                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
+                                <div className="work-half--item fade-effect">
+                                    <div className="hover-effect">
+                                        <a href="https://www.thequint.com/quintlab/what-sanju-missed-about-sanjay-dutts-life/" target="_blank" rel="noreferrer nofollow">
+                                            <img src="./images/port-amp-2.jpg" loading="lazy" alt="" />
+                                            <div className="work-overlay">
+                                                <div className="work-overlay--content">
+                                                    <h4 className="work-item--text item-title">Sanjay Dutt Life - AMP Story</h4>
+                                                    <h5 className="work-item--text item-description">Develop component-based layout,  Card based layout, Accelerated Mobile Pages (AMP).</h5>
+                                                    <button className="work-item--text item-btn" type="button" title="SandeshtoaSoldier">View Project</button>
                                                 </div>
                                             </div>
                                         </a>
