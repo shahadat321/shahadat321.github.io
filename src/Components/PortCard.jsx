498442f5a6e21d81e151e8d580a08744cbc69adb
Shahadat Hussain <shussain@legalsimpli.com>
2022-03-26 00:14:50 +0530

    New UI of Portfolio and added link icon

--- src/Components/PortCard.jsx
+++ src/Components/PortCard.jsx
@@ -1,11 +1,11 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { Scene } from "react-scrollmagic";
 
 class PortCard extends Component{
     render(){
         return(
             <Scene classToggle="fade-up" reverse={true} offset={-300}>
-                <div className="work-half--item fade-effect">
+                <div className="work-thumbs--item fade-effect">
                     <div className="hover-effect">
                         <a href={this.props.href} target="_blank" rel="noreferrer nofollow">
                             <figure>
@@ -23,6 +23,9 @@ class PortCard extends Component{
                                     </div>
                                 </figcaption>
                             </figure>
+                            <span className="link--icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"/></svg>
+                            </span>
                         </a>
                     </div>
                 </div>
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React, { Component, useState } from "react";
 import { Scene } from "react-scrollmagic";
 import Port from "./PortAPI";
-// import PortCard from "./PortCard";
 import PortCard from "./PortCard";
 
 class Portfolio extends Component{
@@ -11,69 +10,8 @@ class Portfolio extends Component{
                 <div className="wrapper">
                     <div className="wrapper--push work-push-up">
 
-                        {/* <div className="work-fluid work-fluid--first">
-                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
-                                <div className="work-fluid--item hover-effect fade-effect">
-                                    <a href="https://pdfsimpli.com/" target="_blank" rel="noreferrer nofollow">
-                                        <img src="./images/port-1.jpg" loading="lazy" alt="" />
-                                        <div className="work-overlay">
-                                            <div className="work-overlay--content">
-                                                <h5 className="work-item--text item-title">PDFSimpli.com</h5>
-                                                <h5 className="work-item--text item-description">Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.</h5>
-                                                <button className="work-item--text item-btn" type="button" title="PDFSimpli.com">View Project</button>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </Scene>
-                        </div> */}
-
-                        <div className="work-half">
-
+                        <div className="work-thumbs">
                             {Port.map((data) => (<PortCard {...data} key={data.id} />))}
-
-                            {/* <Scene classToggle="fade-up" reverse={true} offset={-300}>
-                                <div className="work-half--item fade-effect">
-                                    <div className="hover-effect">
-                                        <a href="https://www.thequint.com/" target="_blank" rel="noreferrer nofollow">
-                                            <figure>
-                                                <picture>
-                                                    <source type="image/webp" srcset="./images/webp/port-2.webp" />
-                                                    <source type="image/jpeg" srcset="./images/port-2.jpg" />
-                                                    <img src="./images/port-2.jpg" />
-                                                </picture>
-                                                <figcaption className="work-overlay">
-                                                    <div className="work-overlay--content">
-                                                        <h4 className="work-item--text item-title">Quint Digital Media Limited</h4>
-                                                        <h5 className="work-item--text item-description">Created component-based layout, JavaScript library ReactJS, Grids susy grids, CSS pre-processor SCSS/SASS.</h5>
-                                                        <button className="work-item--text item-btn" type="button" title="TheQuint.com">View Project</button>
-                                                    </div>
-                                                </figcaption>
-                                            </figure>
-                                            
-                                        </a>
-                                    </div>
-
-                                </div>
-                            </Scene>
-
-                            <Scene classToggle="fade-up" reverse={true} offset={-300}>
-                                <div className="work-half--item fade-effect">
-                                    <div className="hover-effect">
-                                        <a href="https://www.thequint.com/" target="_blank" rel="noreferrer nofollow">
-                                            <img src="./images/port-2.jpg" loading="lazy" alt="" />
-                                            <div className="work-overlay">
-                                                <div className="work-overlay--content">
-                                                    <h4 className="work-item--text item-title">Quint Digital Media Limited</h4>
-                                                    <h5 className="work-item--text item-description">Created component-based layout, JavaScript library ReactJS, Grids susy grids, CSS pre-processor SCSS/SASS.</h5>
-                                                    <button className="work-item--text item-btn" type="button" title="TheQuint.com">View Project</button>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-
-                                </div>
-                            </Scene> */}
                         </div>
 
                         <Scene classToggle="fade-up" reverse={true} offset={-300}>
